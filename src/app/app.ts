5c05366e846378b024b1c3ec882fa09d4ad630c2
Andrew Pethoud <andrewpethoud@gmail.com>
2025-11-24 14:48:16 -0800

    add basic input with autocomplete

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { timeOptions } from './times';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AsyncPipe } from '@angular/common';
+import { map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('timepicker-poc');
+export class App implements OnInit {
+  myControl = new FormControl('');
+  options: string[] = timeOptions;
+  filteredOptions?: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value || ''))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter((option) => option.toLowerCase().includes(filterValue));
+  }
 }
---
+++ src/app/times.ts
@@ -0,0 +1,27 @@
+// export const timeOptions = [
+//   "0100",
+//   "0101",
+//   "0102",
+//   "0159",
+//   "0200",
+//   "0201"
+// ]
+
+const generateTimeOptions = () => {
+  return [
+    '0100',
+    '0101',
+    '0102',
+    '0103',
+    '0104',
+    '0105',
+    '0200',
+    '0202',
+    '0202',
+    '0203',
+    '0204',
+    '0205',
+  ];
+};
+
+export const timeOptions = generateTimeOptions();
